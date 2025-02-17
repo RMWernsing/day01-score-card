1e72aef70ec93e8f4dbea06edf0cacf7ae017a7a
Roman Wernsing <wernsingroman@gmail.com>
2025-02-17 16:05:51 -0700

    buttons work, score doesn't go below 0, reset button is working

--- app.js
+++ app.js
@@ -2,16 +2,68 @@
 // SECTION Global Variables
 
 let homeScore = 0
+let awayScore = 0
 
 // !SECTION 
 
 // SECTION Logic
 
-function addScore1() {
+// NOTE home
+
+function homeAddScore1() {
   homeScore += 1
   console.log(homeScore);
+  drawScoreHome()
 
 }
+
+function homeAddScore3() {
+  homeScore += 3
+  console.log(homeScore);
+  drawScoreHome()
+}
+
+function homeSubScore1() {
+  homeScore -= 1
+  if (homeScore <= 0) {
+    homeScore = 0
+  }
+  console.log(homeScore);
+  drawScoreHome()
+}
+
+
+// NOTE away
+
+function awayAddScore1() {
+  awayScore += 1
+  console.log(awayScore);
+  drawScoreAway()
+}
+
+function awayAddScore3() {
+  awayScore += 3
+  console.log(awayScore);
+  drawScoreAway()
+}
+
+function awaySubScore1() {
+  awayScore -= 1
+  if (awayScore <= 0) {
+    awayScore = 0
+  }
+  console.log(awayScore);
+  drawScoreAway()
+}
+
+
+function reset() {
+  awayScore = 0
+  homeScore = 0
+  drawScoreAway()
+  drawScoreHome()
+}
+
 
 
 
@@ -20,10 +72,19 @@ function addScore1() {
 
 // SECTION Drawing
 
-function drawScore1() {
-  const addScoreH2Elem = document.getElementById('addScoreH2')
+function drawScoreHome() {
+  const addScoreHomeElem = document.getElementById('addScoreHome')
+  addScoreHomeElem.innerText = homeScore
+}
+
+function drawScoreAway() {
+  const addScoreAwayElem = document.getElementById('addScoreAway')
+  addScoreAwayElem.innerText = awayScore
 
 }
+
+
+
 
 // !SECTION
 
