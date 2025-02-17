a084662ce8180850bb722390182d40ba2135c736
Roman Wernsing <wernsingroman@gmail.com>
2025-02-17 15:14:26 -0700

    added buttons, rough start with the layout, starting to add java script functions

---
+++ app.js
@@ -0,0 +1,29 @@
+// ANCHOR application code
+// SECTION Global Variables
+
+let homeScore = 0
+
+// !SECTION 
+
+// SECTION Logic
+
+function addScore1() {
+  homeScore += 1
+  console.log(homeScore);
+
+}
+
+
+
+
+// !SECTION
+
+// SECTION Drawing
+
+function drawScore1() {
+  const addScoreH2Elem = document.getElementById('addScoreH2')
+
+}
+
+// !SECTION
+
